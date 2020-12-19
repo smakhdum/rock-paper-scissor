ef41688aa98640904e42352b819c40a25054425f
makhdum <s.makhdum.u@gmail.com>
2020-12-19 16:51:40 +0600

    rock-paper-sessor commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Sayed Makhdum Ullah-  Rock Paper Scissors</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<h1>Sayed Makhdum Ullah- Welcome to Rock Paper Scissors</h1>
+<p><strong>Enjoyyy!!</strong> 
+</p>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+Attempt to go to
+<a href="game.php">game.php</a> without logging in - it should fail with an error message.
+<p>
+<a href="http://www.wa4e.com/code/rps.zip"
+ target="_blank">Source Code for this Application</a>
+</p>
+</div>
+</body>
